77fe9984186b5eb63750cccf7ad273181437ebb8
nflamously <david.dembinski.1996@gmail.com>
2025-02-23 00:22:37 +0100

    feat: override glinerdataset original stuf

--- train.py
+++ train.py
@@ -1,82 +1,16 @@
 import ast
 import re
-from random import random
-from typing import Optional
 
-from gliner import GLiNER, GLiNERConfig
-from gliner.data_processing import DataCollator, GLiNERDataset, WordsSplitter, TokenProcessor, SpanProcessor
-from transformers import TrainingArguments, Trainer, DataCollatorForTokenClassification, AutoTokenizer
-from datasets import load_dataset, tqdm, Dataset
+from gliner import GLiNER
+from gliner.data_processing import DataCollator, GLiNERDataset, WordsSplitter
+from transformers import TrainingArguments, Trainer, AutoTokenizer
+from datasets import load_dataset
 
 
-class CustomGLiNERDataset(Dataset):
-    def __init__(self, examples,
-                 config: Optional[GLiNERConfig],
-                 tokenizer: Optional[AutoTokenizer] = None,
-                 words_splitter: Optional[WordsSplitter] = None,
-                 data_processor=None,
-                 entities=None,
-                 get_negatives: bool = True):
-        self._data = examples
-        self.config = config
-        if data_processor is not None:
-            self.data_processor = data_processor
-        else:
-            if config.span_mode == "token_level":
-                self.data_processor = TokenProcessor(config, tokenizer, words_splitter, preprocess_text=True)
-            else:
-                self.data_processor = SpanProcessor(config, tokenizer, words_splitter, preprocess_text=True)
-
-        self.max_neg_type_ratio = int(self.config.max_neg_type_ratio)
-        self.get_negatives = get_negatives
-        if not entities:
-            self.all_entities = self._collect_all_entities()
-        else:
-            self.all_entities = entities
-        self.max_negatives = min(50, len(self.all_entities))
-
+class CustomGLiNERDataset(GLiNERDataset):
     def _get_entities_from_example(self, example):
         entities = {ner["key"] for ner in example['ner']}
         return entities
-
-    def _collect_all_entities(self):
-        print("Collecting all entities...")
-        all_entities = set()
-        for example in tqdm(self._data):
-            curr_entities = self._get_entities_from_example(example)
-            all_entities.update(curr_entities)
-        print('Total number of entity classes: ', len(all_entities))
-        return list(all_entities)
-
-    def _get_negatives(self):
-        negatives = random.sample(self.all_entities, k=self.max_negatives)
-        random.shuffle(negatives)
-        return negatives
-
-    def __len__(self):
-        return len(self._data)
-
-    def __getitem__(self, idx):
-        try:
-            example = self._data[idx]
-            if self.get_negatives:
-                curr_negatives = self._get_negatives()
-            else:
-                curr_negatives = None
-
-            raw_batch = self.data_processor.collate_raw_batch([example], negatives=curr_negatives)
-
-            model_input = self.data_processor.collate_fn(raw_batch, prepare_labels=True)
-            if 'span_idx' in raw_batch:
-                model_input['span_idx'] = raw_batch['span_idx']
-            if 'span_mask' in raw_batch:
-                model_input['span_mask'] = raw_batch['span_mask']
-            if 'seq_length' in raw_batch:
-                model_input['text_lengths'] = raw_batch['seq_length']
-            return model_input
-        except Exception as e:
-            print(f"Skipping getting item due to error: {e}")
-            return None
 
 
 if __name__ == "__main__":
@@ -152,7 +86,7 @@ if __name__ == "__main__":
     # 5. Set up training arguments.
     num_steps = 500
     batch_size = 8
-    data_size = len(ds["full"])
+    data_size = len(train_dataset)
     num_batches = data_size // batch_size
     num_epochs = max(1, num_steps // num_batches)
 
@@ -160,8 +94,8 @@ if __name__ == "__main__":
         output_dir="logs",
         learning_rate=1e-5,
         weight_decay=0.1,
-        others_lr=3e-5,
-        others_weight_decay=0.01,
+        #others_lr=3e-5,
+        #others_weight_decay=0.01,
         # focal_loss_gamma=config.loss_gamma,
         # focal_loss_alpha=config.loss_alpha,
         lr_scheduler_type="cosine",
@@ -183,8 +117,9 @@ if __name__ == "__main__":
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        eval_dataset=test_dataset,
+        # eval_dataset=test_dataset,
         tokenizer=tokenizer,
         data_collator=data_collator,
     )
+
     trainer.train()
